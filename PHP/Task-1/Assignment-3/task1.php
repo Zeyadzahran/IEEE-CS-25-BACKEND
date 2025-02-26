1d788e2e2dc9481140c3f5187493ac6ece01b935
Zeyad Zahran <155586428+Zeyadzahran@users.noreply.github.com>
2025-02-26 04:09:40 +0200

    Create task1.php

---
+++ PHP/Task-1/Assignment-3/task1.php
@@ -0,0 +1,21 @@
+<?php
+  $courseName = "Elzero Courses";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo $courseName; ?>">
+    <title>Welcome To <?php echo $courseName; ?></title>
+  </head>
+  <body>
+    <h1><?php echo $courseName; ?></h1>
+    <p>Here In <?php echo $courseName; ?> We Provide Front-End And Back-End Courses</p>
+    <hr>
+    <div><?php echo $courseName; ?> Is The What You Need.</div>
+    <footer>All Right Reserved To <?php echo $courseName; ?></footer>
+  </body>
+</html>
