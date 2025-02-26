6c067cf2db4a5ccba1c6f25eef48fb12c1e41090
Zeyad Zahran <155586428+Zeyadzahran@users.noreply.github.com>
2025-02-26 03:03:07 +0200

    Create task1.php

---
+++ PHP/Task-1/Assignment-1/task1.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * bro i really hate my self
+ * this is task 1 assignement 1  
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+      <?php
+      echo "MY first PHP";
+      ?>
+    </title>
+  </head>
+  <body>
+    <div>
+      <?php
+      echo "I love myself"; 
+      ?>
+      <br>
+    </div>
+    <div>
+      <?php
+      echo "MY self, yeah, I am so arrogant"; 
+      ?>
+      <br>
+    </div>
+  </body>
+</html>
